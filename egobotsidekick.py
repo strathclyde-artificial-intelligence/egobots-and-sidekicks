d442da6e9d4f9a37116baf0163f119c8dd4c86eb
Mark McArthur <56350432+Roedhip@users.noreply.github.com>
2021-10-01 06:05:10 +0100

    Rearranging and Updating
    The problem files for the inspect only case have been moved into a single folder and the up to date, working version of the python script is uploaded.

--- egobotsidekick.py
+++ egobotsidekick.py
@@ -3,18 +3,19 @@
 # This file takes an egobot domain, factored egobot problems, a sidekick domain, an empty sidekick problem, and a planner as input.
 # This file generates files for each intermediate plan and problem.
 # This file generates the final plans.
+# This file can currently only handle inspect goals.
 
 import os
 
 # Here strings are defined which are used to find and create files.
 egopt1 = 'egobot-'
 egopt2 = '-problem-'
-egopt3 = 'Egobot '
-egopt4 = ' Iteration '
+egopt3 = 'Egobot-'
+egopt4 = '-Iteration-'
 egolist = ['1','2','3','4']
 
 sidpt1 = 'sidekick-problem-'
-sidpt2 = 'Sidekick Iteration '
+sidpt2 = 'Sidekick-Iteration-'
 
 # Here the empty sidekick problem is named.
 sidfileempty = 'sidekick-problem-empty.pddl'
@@ -24,7 +25,7 @@ f.close()
 
 # Here the domains are named.
 egodomain = 'maintenance-domain.pddl'
-siddomain = ''
+siddomain = 'sidekick-domain-1.pddl'
 
 # Here the planner is named.
 planner = 'optic-cplex -n'
@@ -48,9 +49,11 @@ for x in egolist:
 
 # Here the egobot problems are run for the first time.
 egoplan = []
-timeout = 'timeout 2 '
+timeout = 'timeout 10 '
 i = 0
 for x in egobotproblemfiles:
+    f = open(egobotplanfiles[i],'x')
+    f.close()
     arg = timeout+'./'+planner+' '+egodomain+' '+x+' > '+egobotplanfiles[i]
     os.system(arg)
     f = open(egobotplanfiles[i],'r')
@@ -105,7 +108,7 @@ for i, x in enumerate(egoplan):
 if all(egosuccess): # In theory if there are any 0s in the list, it should return False
     success = 1
 
-# Here the looping starts. There is not yet a way for this loop to end after a limit of iterations. There is not yet a way for this loop to recognise success.
+# Here the looping starts. There is not yet a way for this loop to end after a limit of iterations.
 while success == 0:
     # Here the inspect demands are formatted for the sidekick.
     tempstr2 = '\n'
@@ -129,7 +132,7 @@ while success == 0:
         tempint1.append(len(x))
     minscore = max(tempint1) - 1
     minscorestr = str(minscore)
-    tempstr4 = tempstr1[0]+tempstr1[1]+allinspdemandspddl+tempstr2[0]+minscorestr+tempstr2[2]
+    tempstr4 = tempstr1[0]+tempstr1[1]+allinspdemandspddl+tempstr2[0]+tempstr2[1]+tempstr3[0]+minscorestr+tempstr3[2]
     sidproblem = tempstr4
 
     # Here the sidekick problem is saved as a file.
@@ -139,8 +142,10 @@ while success == 0:
     f.close()
 
     # Here the sidekick problem is run.
-    timeout = 'timeout 5 '
+    timeout = 'timeout 20 '
     sidplanfile = sidpt2+iterstr+'.txt'
+    f = open(sidplanfile, 'x')
+    f.close()
     arg = timeout+'./'+planner+' '+siddomain+' '+sidfile+' > '+sidplanfile
     os.system(arg)
     f = open(sidplanfile, 'r')
@@ -155,7 +160,13 @@ while success == 0:
     tempstr1 = sidplan.split('0.000:')
     tempstr2 = tempstr1[-1]
     tempstr3 = '0.000:'+tempstr2
-    tempstr4 = tempstr3.splitlines()
+    tempstr3b = tempstr3.split('[1.000]\n')
+    tempstr3c = ''
+    for x in tempstr3b:
+        if ': (' in x:
+            tempstr3c = tempstr3c + '\n'+ x + '[1.000]'
+    print('tempstr3c is: ' +tempstr3c)
+    tempstr4 = tempstr3c.splitlines()
 
     tempstr5 = []
     tempstr6 = []
@@ -163,6 +174,8 @@ while success == 0:
     tempstr8 = []
     for x in tempstr4:
         if 'pn1' in x: # This if statement should be refined to handle n many egobots
+            # tempdict['pn1'] = [] # if its not created yet.
+            # tempdict['pn1'].append(x)
             tempstr5.append(x)
         elif 'pn2' in x:
             tempstr6.append(x)
@@ -171,55 +184,90 @@ while success == 0:
         elif 'pn4' in x:
             tempstr8.append(x)
     
+    print('tempstr5 is: ')
+    for x in tempstr5:
+        print(x)
+    print('tempstr6 is: ')
+    for x in tempstr6:
+        print(x)
+    print('tempstr7 is: ')
+    for x in tempstr7:
+        print(x)
+    print('tempstr8 is: ')
+    for x in tempstr8:
+        print(x)
+
     tempstr9 = tempstr4[-1].partition(':')
+    print('tempstr9 is: ')
+    for x in tempstr9:
+        print(x)
     tempstr10 = tempstr9[0] # extracting the time the final action begins
+    print('tempstr10 is: ' + tempstr10)
     tempstr11 = tempstr4[-1].partition('l') # cutting at the sidekick's final location
+    print('tempstr11 is: ')
+    for x in tempstr11:
+        print(x)
     tempstr12 = tempstr11[2].partition(' ') # cutting off all data except location data
+    print('tempstr12 is: ')
+    for x in tempstr12:
+        print(x)
     tempstr13 = tempstr11[1]+tempstr12[0] # extracting final location
+    print('tempstr13 is: ' + tempstr13)
 
     sidplanend = str(float(tempstr10) + 1.0 + timeoffset) # add 1.0 because all actions take 1.0, this will need to be improved
+    print('sidplanend is: ' + sidplanend)
     sidplanloc = tempstr13
-    timeoffset = sidplanend
+    timeoffset = float(sidplanend)
 
     egoactions = [tempstr5, tempstr6, tempstr7, tempstr8]
     egopanels = [[],[],[],[]]
 
-    for i, x in enumerate(egoactions): # unsure if I have used enumerate correctly
+    for i, x in enumerate(egoactions):
         tempstr1 = []
         for y in x:
             tempstr2 = y.partition('pn')
             tempstr3 = tempstr2[2].partition(' ')
             tempstr1.append(tempstr2[1]+tempstr3[0])
         egopanels[i] = tempstr1
+        print('egopanels ' + str(i) + ' is: ')
+        for z in egopanels[i]:
+            print(z)
 
     # Here the egobot problems are adjusted
-    for i, x in enumerate(egobotproblemfiles): # unsure if I have used enumerate correctly.2
+    for i, x in enumerate(egobotproblemfiles):
         f = open(x, 'r')
         tempstr1 = f.read()
         f.close()
+        print('tempstr1 is: ' + tempstr1)
         
         tempstr2 = tempstr1.splitlines(True)
         tempstr3 = ''
         for y in tempstr2:
             if '(at sid ' in y:
                 y = '(at '+sidplanend+' (at sid '+sidplanloc+')\n'
+                print('Line changed to:' + y)
             for z in egopanels[i]:
                 if z in y: # if panel is in line of egobot problem
-                    if 'is-inspected' in x:
+                    if 'is-inspected' in y:
                         y = ';' + y
+                        print('Line changed to:' + y)
             tempstr3 = tempstr3 + y
 
-        x = egopt1+egolist[i]+egopt2+iterstr+'.pddl'
+        print('tempstr3 is ' + tempstr3)
+
+        egobotproblemfiles[i] = egopt1+egolist[i]+egopt2+iterstr+'.pddl'
         egobotplanfiles[i] = egopt3+egolist[i]+egopt4+iterstr+'.txt'
 
-        f = open(x, 'x')
-        f.write(x)
+        f = open(egobotproblemfiles[i], 'x')
+        f.write(tempstr3)
         f.close()
     
     # Here the egobot problems are run.
-    timeout = 'timeout 2 '
-    for i, x in enumerate(egobotproblemfiles): # unsure if I have used enumerate correctly.3
+    timeout = 'timeout 10 '
+    for i, x in enumerate(egobotproblemfiles):
         if egosuccess[i] == 0:
+            f = open(egobotplanfiles[i],'x')
+            f.close()
             arg = timeout+'./'+planner+' '+egodomain+' '+x+' > '+egobotplanfiles[i]
             os.system(arg)
             f = open(egobotplanfiles[i],'r')
@@ -235,45 +283,46 @@ while success == 0:
     egowelddemands = []
     egoptchdemands = []
     for i, x in enumerate(egoplan):
-        tempstr1 = x.split('0.000:')
-        tempstr2 = tempstr1[-1]
-        tempstr3 = '0.000:'+tempstr2
-        tempstr4 = tempstr3.splitlines()
-        tempstr5 = []
-        for y in tempstr4:
-            if 'sid' in y:
-                tempstr5.append(y)
+        if isinstance(x, str):
+            tempstr1 = x.split('0.000:')
+            tempstr2 = tempstr1[-1]
+            tempstr3 = '0.000:'+tempstr2
+            tempstr4 = tempstr3.splitlines()
+            tempstr5 = []
+            for y in tempstr4:
+                if 'sid' in y:
+                    tempstr5.append(y)
 
-        if tempstr5 == []:
-            egosuccess[i] = 1
-        else:
-            tempstr6 = []
-            for y in tempstr5:
-                if 'inspect' in y:
-                    tempstr6.append(y)
+            if tempstr5 == []:
+                egosuccess[i] = 1
+            else:
+                tempstr6 = []
+                for y in tempstr5:
+                    if 'inspect' in y:
+                        tempstr6.append(y)
 
-            egoinspdemands.append(tempstr6)
+                egoinspdemands.append(tempstr6)
 
-            tempstr7 = []
-            for y in tempstr5:
-                if 'drop' in y:
-                    tempstr7.append(y)
+                tempstr7 = []
+                for y in tempstr5:
+                    if 'drop' in y:
+                        tempstr7.append(y)
 
-            tempstr8 = []
-            for y in tempstr7:
-                if 'w' in y:
-                    tempstr8.append(y)
+                tempstr8 = []
+                for y in tempstr7:
+                    if 'w' in y:
+                        tempstr8.append(y)
 
-            egowelddemands.append(tempstr8)
+                egowelddemands.append(tempstr8)
 
-            tempstr9 = []
-            for y in tempstr7:
-                if 'pa' in y:
-                    tempstr9.append(y)
+                tempstr9 = []
+                for y in tempstr7:
+                    if 'pa' in y:
+                        tempstr9.append(y)
 
-            egoptchdemands.append(tempstr9)
+                egoptchdemands.append(tempstr9)
 
     if all(egosuccess): # In theory if there are any 0s in the list, it should return False
         success = 1
 
-# Here the script is done. I don't technically need any code here I guess (for now at least).
+# Could add a message here about reaching success.
