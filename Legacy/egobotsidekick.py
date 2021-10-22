ac3ca071f5f5cf032f008d15f86f95581017cdd0
Mark McArthur <56350432+Roedhip@users.noreply.github.com>
2021-10-22 14:57:07 +0100

    Rearranging Files, Adding Updated Domains

---
+++ Legacy/egobotsidekick.py
@@ -0,0 +1,328 @@
+# egobotsidekick.py
+# This file performs the entire egobots sidekicks process for the inspect, weld, patch domain.
+# This file takes an egobot domain, factored egobot problems, a sidekick domain, an empty sidekick problem, and a planner as input.
+# This file generates files for each intermediate plan and problem.
+# This file generates the final plans.
+# This file can currently only handle inspect goals.
+# This file requires that none of the auto-generated file names are already in use.
+
+import os
+
+# Here strings are defined which are used to find and create files.
+egopt1 = 'egobot-'
+egopt2 = '-problem-'
+egopt3 = 'Egobot-'
+egopt4 = '-Iteration-'
+egolist = ['1','2','3','4']
+
+sidpt1 = 'sidekick-problem-'
+sidpt2 = 'Sidekick-Iteration-'
+
+# Here the empty sidekick problem is named.
+sidfileempty = 'sidekick-problem-empty-2.pddl'
+f = open(sidfileempty,'r')
+sidempty = f.read()
+f.close()
+
+# Here the domains are named.
+egodomain = 'maintenance-domain.pddl'
+siddomain = 'sidekick-domain-2.pddl'
+
+# Here the planner is named.
+planner = 'optic-cplex -n'
+
+# Here the iteration counting variable and the success variable are made.
+iter = 1
+iterstr = str(iter)
+success = 0
+egosuccess = []
+for x in egolist:
+    egosuccess.append(0)
+
+timeoffset = 0.0
+
+# Here the egobot input file names are found.
+egobotproblemfiles = []
+egobotplanfiles = []
+for x in egolist:
+    egobotproblemfiles.append(egopt1+x+egopt2+iterstr+'.pddl')
+    egobotplanfiles.append(egopt3+x+egopt4+iterstr+'.txt')
+
+# Here the egobot problems are run for the first time.
+egoplan = []
+timeout = 'timeout 10 '
+i = 0
+for x in egobotproblemfiles:
+    f = open(egobotplanfiles[i],'x')
+    f.close()
+    arg = timeout+'./'+planner+' '+egodomain+' '+x+' > '+egobotplanfiles[i]
+    os.system(arg)
+    f = open(egobotplanfiles[i],'r')
+    tempstr1 = f.read()
+    egoplan.append(tempstr1)
+    f.close()
+    i = i+1
+
+# Here the first set of egobot plans are parsed.
+egodemands = [] # These are lists of lists.
+for i, x in enumerate(egoplan):
+    tempstr1 = x.split('0.000:')
+    tempstr2 = tempstr1[-1]
+    tempstr3 = '0.000:'+tempstr2
+    tempstr4 = tempstr3.splitlines()
+    tempstr5 = []
+    for y in tempstr4:
+        if 'sid' in y:
+            tempstr5.append(y)
+
+    if tempstr5 == []:
+        egosuccess[i] = 1
+    else:
+        tempstr6 = []
+        for y in tempstr5:
+            if 'inspect' or 'drop' in y:
+                tempstr6.append(y)
+
+        egodemands.append(tempstr6)
+
+# Add parsing for where welders and patches end up
+
+if all(egosuccess): # In theory if there are any 0s in the list, it should return False
+    success = 1
+
+# Here the looping starts. There is not yet a way for this loop to end after a limit of iterations.
+while success == 0:
+    # Here the inspect demands are formatted for the sidekick.
+    tempstr2 = '\n'
+    for x in egodemands:
+        tempstr1 = x[-1].partition(':')
+        deadline = tempstr1[0]
+        for y in x:
+            if 'inspect' in y:
+                tempstr3 = y.partition('pn')
+                tempstr4 = tempstr3[2].partition(' ')
+                panel = tempstr3[1]+tempstr4[0]
+                tempstr5 = '\n'+'(= (ingoal '+panel+') 1)'+'\n'+'(is-not-inspected '+panel+')'+'\n'+'(at '+deadline+' (not (is-not-inspected '+panel+')))'
+                tempstr2 = tempstr2 + tempstr5
+            elif 'drop' in y:
+                if 'w' in y:
+                    tempstr3 = y.partition(' l') # partition at location
+                    tempstr4 = tempstr3[2].partition(')')
+                    droplocation = 'l'+tempstr4[0]
+                    tempstr5 = '\n'+'(= (wegoal '+droplocation+') 10)'+'\n'+'(welder-drop-needed '+droplocation+')'+'\n'+'(at '+deadline+' (not (welder-drop-needed '+droplocation+')))'
+                if 'pa' in y: #currently just same as welder, must be updated
+                    tempstr3 = y.partition(' l') # partition at location
+                    tempstr4 = tempstr3[2].partition(')')
+                    droplocation = 'l'+tempstr4[0]
+                    tempstr5 = '\n'+'(= (wegoal '+droplocation+') 10)'+'\n'+'(welder-drop-needed '+droplocation+')'+'\n'+'(at '+deadline+' (not (welder-drop-needed '+droplocation+')))'
+    alldemandspddl = tempstr2
+
+    # Add welder and patch demands here.
+
+    # Here the empty sidekick problem is modified to include inspect requests.
+    tempstr1 = sidempty.partition('(deadline-open)')
+    tempstr2 = tempstr1[2].partition('(> (score) ')
+    tempstr3 = tempstr2[2].partition('0')
+    tempint1 = []
+    for x in egodemands:
+        tempint1.append(len(x))
+    minscore = max(tempint1) - 1
+    minscorestr = str(minscore)
+    tempstr4 = tempstr1[0]+tempstr1[1]+alldemandspddl+tempstr2[0]+tempstr2[1]+tempstr3[0]+minscorestr+tempstr3[2]
+    sidproblem = tempstr4
+
+    # Here the sidekick problem is saved as a file.
+    sidfile = sidpt1+iterstr+'.pddl'
+    f = open(sidfile,'x')
+    f.write(sidproblem)
+    f.close()
+
+    # Here the sidekick problem is run.
+    timeout = 'timeout 20 '
+    sidplanfile = sidpt2+iterstr+'.txt'
+    f = open(sidplanfile, 'x')
+    f.close()
+    arg = timeout+'./'+planner+' '+siddomain+' '+sidfile+' > '+sidplanfile
+    os.system(arg)
+    f = open(sidplanfile, 'r')
+    sidplan = f.read()
+    f.close()
+
+    # Here the iteration number is increased
+    iter = iter+1
+    iterstr = str(iter)
+
+    # Here the sidekick problem is parsed for inspect actions.
+    tempstr1 = sidplan.split('0.000:')
+    tempstr2 = tempstr1[-1]
+    tempstr3 = '0.000:'+tempstr2
+    tempstr3b = tempstr3.split('[1.000]\n')
+    tempstr3c = ''
+    for x in tempstr3b:
+        if ': (' in x:
+            tempstr3c = tempstr3c + '\n'+ x + '[1.000]'
+    #print('tempstr3c is: ' +tempstr3c)
+    tempstr4 = tempstr3c.splitlines()
+
+    tempstr5 = []
+    tempstr6 = []
+    tempstr7 = []
+    tempstr8 = []
+    tempwelderdrops1 = [''] #this might avoid formatting errors later, as there is always a string for the in command to refer to
+    tempwelderdrops2 = ['']
+    tempwelderdrops3 = ['']
+    tempwelderdrops4 = ['']
+    for x in tempstr4:
+        if 'pn1' in x: # This if statement should be refined to handle n many egobots
+            # tempdict['pn1'] = [] # if its not created yet.
+            # tempdict['pn1'].append(x)
+            tempstr5.append(x)
+        elif 'pn2' in x:
+            tempstr6.append(x)
+        elif 'pn3' in x:
+            tempstr7.append(x)
+        elif 'pn4' in x:
+            tempstr8.append(x)
+        elif 'w' and 'drop' in x:
+            if 'l1' in x:
+                tempwelderdrops1.append(x)
+            elif 'l2' in x:
+                tempwelderdrops2.append(x)
+            elif 'l3' in x:
+                tempwelderdrops3.append(x)
+            elif 'l4' in x:
+                tempwelderdrops4.append(x)
+    
+    #print('tempstr5 is: ')
+    #for x in tempstr5:
+    #    print(x)
+    #print('tempstr6 is: ')
+    #for x in tempstr6:
+    #    print(x)
+    #print('tempstr7 is: ')
+    #for x in tempstr7:
+    #    print(x)
+    #print('tempstr8 is: ')
+    #for x in tempstr8:
+    #    print(x)
+
+    tempstr9 = tempstr4[-1].partition(':')
+    #print('tempstr9 is: ')
+    #for x in tempstr9:
+    #    print(x)
+    tempstr10 = tempstr9[0] # extracting the time the final action begins
+    #print('tempstr10 is: ' + tempstr10)
+    tempstr11 = tempstr4[-1].partition('l') # cutting at the sidekick's final location
+    #print('tempstr11 is: ')
+    #for x in tempstr11:
+    #    print(x)
+    tempstr12 = tempstr11[2].partition(' ') # cutting off all data except location data
+    #print('tempstr12 is: ')
+    #for x in tempstr12:
+    #    print(x)
+    tempstr13 = tempstr11[1]+tempstr12[0] # extracting final location
+    #print('tempstr13 is: ' + tempstr13)
+
+    sidplanend = str(float(tempstr10) + 1.0 + timeoffset) # add 1.0 because all actions take 1.0, this will need to be improved
+    #print('sidplanend is: ' + sidplanend)
+    sidplanloc = tempstr13
+    timeoffset = float(sidplanend)
+
+    egoactions = [tempstr5, tempstr6, tempstr7, tempstr8]
+    egopanels = [[],[],[],[]]
+
+    for i, x in enumerate(egoactions):
+        tempstr1 = []
+        for y in x:
+            tempstr2 = y.partition('pn')
+            tempstr3 = tempstr2[2].partition(' ')
+            tempstr1.append(tempstr2[1]+tempstr3[0])
+        egopanels[i] = tempstr1
+        #print('egopanels ' + str(i) + ' is: ')
+        #for z in egopanels[i]:
+        #    print(z)
+    
+    egowelder = [tempwelderdrops1, tempwelderdrops2, tempwelderdrops3, tempwelderdrops4]
+    egowelderdroploc = [[],[],[],[]]
+
+    for i, x in enumerate(egowelder):
+        tempstr1 = []
+        for y in x:
+            if 'w' in y:
+                tempstr2 = y.partition(' l') # partition at location
+                tempstr3 = tempstr2[2].partition(')')
+                droplocation = 'l'+tempstr3[0]
+                tempstr1.append(droplocation)
+        egowelderdroploc[i] = tempstr1 #unsure if this will break whenever there are no welder drops or not
+    # Add parsing for  patch drops
+
+    # Here the egobot problems are adjusted
+    for i, x in enumerate(egobotproblemfiles):
+        f = open(x, 'r')
+        tempstr1 = f.read()
+        f.close()
+        #print('tempstr1 is: ' + tempstr1)
+        
+        tempstr2 = tempstr1.splitlines(True)
+        tempstr3 = ''
+        for y in tempstr2:
+            if '(at sid ' in y:
+                y = '(at '+sidplanend+' (at sid '+sidplanloc+')\n'
+                #print('Line changed to:' + y)
+            for z in egopanels[i]:
+                if z in y: # if panel is in line of egobot problem
+                    if 'is-inspected' in y:
+                        y = ';' + y
+                        #print('Line changed to:' + y)
+            tempstr3 = tempstr3 + y
+
+        #print('tempstr3 is ' + tempstr3)
+
+        egobotproblemfiles[i] = egopt1+egolist[i]+egopt2+iterstr+'.pddl'
+        egobotplanfiles[i] = egopt3+egolist[i]+egopt4+iterstr+'.txt'
+
+        f = open(egobotproblemfiles[i], 'x')
+        f.write(tempstr3)
+        f.close()
+    
+    # Here the egobot problems are run.
+    timeout = 'timeout 10 '
+    for i, x in enumerate(egobotproblemfiles):
+        if egosuccess[i] == 0:
+            f = open(egobotplanfiles[i],'x')
+            f.close()
+            arg = timeout+'./'+planner+' '+egodomain+' '+x+' > '+egobotplanfiles[i]
+            os.system(arg)
+            f = open(egobotplanfiles[i],'r')
+            tempstr1 = f.read()
+            egoplan[i] = tempstr1
+            f.close()
+        else:
+            egoplan[i] = []
+    
+    # Here the egobot problems are parsed.
+    egodemands = [] # These are lists of lists.
+    for i, x in enumerate(egoplan):
+        tempstr1 = x.split('0.000:')
+        tempstr2 = tempstr1[-1]
+        tempstr3 = '0.000:'+tempstr2
+        tempstr4 = tempstr3.splitlines()
+        tempstr5 = []
+        for y in tempstr4:
+            if 'sid' in y:
+                tempstr5.append(y)
+
+        if tempstr5 == []:
+            egosuccess[i] = 1
+        else:
+            tempstr6 = []
+            for y in tempstr5:
+                if 'inspect' or 'drop' in y:
+                    tempstr6.append(y)
+
+            egodemands.append(tempstr6)
+
+    if all(egosuccess): # In theory if there are any 0s in the list, it should return False
+        success = 1
+
+# Could add a message here about reaching success.
---
+++ sidekick-domain-3.pddl
@@ -0,0 +1,154 @@
+;Header and description
+
+(define (domain maintenance-domain-gobjects)
+
+;I set it to use durative actions so I can have at start and at end things
+(:requirements :durative-actions :typing :numeric-fluents :timed-initial-literals)
+
+(:types 
+    egobot sidekick - robot
+    location
+    panel
+    patch welder - tool
+)
+
+(:functions
+    (ingoal ?pn - panel)
+    (pagoal ?l - location)
+    (wegoal ?l - location)
+    (score)
+)
+
+;camera-free restricts the robots to only one inspect, patch, weld, pick-up, or drop action at a time
+;hands-free restricts the robots to carrying only one tool at a time
+;hands-free is negated by holding
+;dropped is negated by holding
+;there are two different definitions of adjacency, because sidekick can travel further than egobot
+;issues do not exist, there are simply goals to achieve panel predicates
+;inspection requires no tool and can be done by any robot
+;patching requires a patch, must be done by an egobot, and consumes the patch
+;welding requires a welder, must be done by an egobot, and does not consume the welder
+(:predicates 
+    (hands-free ?r - robot)
+    (camera-free ?r - robot)
+    (at ?r - robot ?l - location)
+    (holding ?r - robot ?t - tool)
+    (dropped ?t - tool ?l - location)
+    (egobot-adjacent ?l1 - location ?l2 - location)
+    (sidekick-adjacent ?l1 - location ?l2 - location)
+    (panel-at ?pn - panel ?l - location)
+    (is-inspected ?pn - panel)
+    (is-not-inspected ?pn - panel)
+    (welder-needed)
+    (patch-needed)
+    (welder-drop-needed ?l - location)
+    (welder-pick-up-valid ?l - location)
+    (patch-drop-needed ?l - location)
+    (is-patched ?pn - panel)
+    (is-welded ?pn - panel)
+
+    (deadline-open)
+)
+
+(:durative-action egobot-move
+    :parameters (?e - egobot ?l1 - location ?l2 - location)
+    :duration (= ?duration 1)
+    :condition (and 
+        (at start (and 
+            (at ?e ?l1)
+        ))
+        (over all (and
+            (egobot-adjacent ?l1 ?l2)
+        ))
+    )
+    :effect (and 
+        (at start (and 
+            (not (at ?e ?l1))
+        ))
+        (at end (and 
+            (at ?e ?l2)
+        ))
+    )
+)
+
+(:durative-action sidekick-move
+    :parameters (?s - sidekick ?l1 - location ?l2 - location)
+    :duration (= ?duration 1)
+    :condition (and 
+        (at start (and 
+            (at ?s ?l1)
+        ))
+        (over all (and
+            (sidekick-adjacent ?l1 ?l2)
+        ))
+    )
+    :effect (and 
+        (at start (and (not (at ?s ?l1))))
+        (at end (and (at ?s ?l2)))
+    )
+)
+
+
+
+(:durative-action inspect
+    :parameters (?r - robot ?pn - panel ?l - location)
+    :duration (= ?duration 1)
+    :condition (and 
+        (at start (camera-free ?r))
+        (at start (deadline-open))
+        (over all (panel-at ?pn ?l))
+        (over all (at ?r ?l))
+        (at start (is-not-inspected ?pn))
+    )
+    :effect (and 
+        (at start (not (is-not-inspected ?pn)))
+        (at start (not (camera-free ?r)))
+        (at end (is-inspected ?pn))
+        (at end  (camera-free ?r))
+        (at end (increase (score) (ingoal ?pn)))
+    )
+)
+
+
+(:durative-action pick-up-welder
+    :parameters (?r - robot ?w - welder ?l - location)
+    :duration (= ?duration 1)
+    :condition (and 
+        (at start (camera-free ?r))
+        (at start (hands-free ?r))
+        (at start (deadline-open))
+        (at start (dropped ?w ?l))
+        (over all (at ?r ?l))
+        (at start (welder-needed))
+        (at start (welder-pick-up-valid ?l))
+    )
+    :effect (and 
+        (at start (not (dropped ?w ?l)))
+        (at start (not (camera-free ?r)))
+        (at start (not (hands-free ?r)))
+        (at end (holding ?r ?w))
+        (at end (camera-free ?r))
+    )
+)
+
+(:durative-action drop-welder
+    :parameters (?r - robot ?w - welder ?l - location)
+    :duration (= ?duration 1)
+    :condition (and 
+        (at start (camera-free ?r))
+        (at start (deadline-open))
+        (at start (holding ?r ?w))
+        (at start (welder-drop-needed ?l))
+        (over all (at ?r ?l))
+    )
+    :effect (and 
+        (at start (not (holding ?r ?w)))
+        (at start (not (camera-free ?r)))
+        (at start (not (hands-free ?r)))
+        (at end (dropped ?w ?l))
+        (at end (camera-free ?r))
+        (at end (hands-free ?r))
+    )
+)
+
+)
